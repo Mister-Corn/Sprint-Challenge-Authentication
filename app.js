df5a70551322c488fa4d193033f541be8284b07b
Mister-Corn <code@ronaldlibago.com>
2018-06-15 05:45:24 -0700

    Project Start

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const { server } = require('./server');
 const mongoose = require('mongoose');
 const port = process.env.PORT || 5000;
+// mLaB
+const { MONGO_URI } = require('./settings');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/auth-users', {
+mongoose.connect(MONGO_URI, {
   useMongoClient: true
-});
+})
+  .then(() => console.log('\n*** Connected to mLabs Mongo Database ***\n'))
+  .catch(err => console.log('Mongo Connection Error:',err));
+
 
 server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
